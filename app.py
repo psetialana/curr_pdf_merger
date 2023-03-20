2c2c6ffb4578ea35e8d2992da46034d9f7c3fed1
Pradana Setialana <pradana.setialana@gmail.com>
2023-03-20 12:12:35 +0700

    change to drive api

--- app.py
+++ app.py
@@ -3,9 +3,102 @@ import gdown
 import os
 import shutil
 import PyPDF2
+import pickle
+from mimetypes import MimeTypes
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+import io
 app = Flask(__name__)
  
- 
+class DriveAPI:
+    global SCOPES
+        
+    # Define the scopes
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    def __init__(self):
+        
+        # Variable self.creds will
+        # store the user access token.
+        # If no valid token found
+        # we will create one.
+        self.creds = None
+
+        # The file token.pickle stores the
+        # user's access and refresh tokens. It is
+        # created automatically when the authorization
+        # flow completes for the first time.
+
+        # Check if file token.pickle exists
+        if os.path.exists('token.pickle'):
+
+            # Read the token from the file and
+            # store it in the variable self.creds
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+
+        # If no valid credentials are available,
+        # request the user to log in.
+        if not self.creds or not self.creds.valid:
+
+            # If token is expired, it will be refreshed,
+            # else, we will request a new one.
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+
+            # Save the access token in token.pickle
+            # file for future usage
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.creds, token)
+
+        # Connect to the API service
+        self.service = build('drive', 'v3', credentials=self.creds)
+
+        # request a list of first N files or
+        # folders with name and id from the API.
+        results = self.service.files().list(
+            pageSize=100, fields="files(id, name)").execute()
+        items = results.get('files', [])
+
+        # print a list of files
+
+        print("Here's a list of files: \n")
+        print(*items, sep="\n", end="\n\n")
+
+    def FileDownload(self, file_id, file_name):
+        request = self.service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+            
+        # Initialise a downloader object to download the file
+        downloader = MediaIoBaseDownload(fh, request, chunksize=204800)
+        done = False
+
+        try:
+            # Download the data in chunks
+            while not done:
+                status, done = downloader.next_chunk()
+
+            fh.seek(0)
+                
+            # Write the received data to the file
+            with open(file_name, 'wb') as f:
+                shutil.copyfileobj(fh, f)
+
+            print("File Downloaded")
+            # Return True if file Downloaded successfully
+            return True
+        except:
+            
+            # Return False if something went wrong
+            print("Something went wrong.")
+            return False
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -23,6 +116,7 @@ def index():
                     os.makedirs(dirName)
             input = data.get('input')
             input = input.split(',')
+            obj = DriveAPI()
             if input:
                 j = 1
                 for i in input:
@@ -30,11 +124,13 @@ def index():
                         i = i.replace("https://drive.google.com/file/d/", "")
                         i = i.replace("/view?usp=drivesdk", "")
                         i = i.replace(" ", "")
-                        output = gdown.download(id=i, output=sheet_id+'_'+str(j)+'.pdf', use_cookies=False, quiet=False)
-                        output = str(output)
-                        if (output is not None):
-                            shutil.move(output, "storage/"+lang+"/"+sheet_id+"/"+output)
-                        j+=1
+                        # output = gdown.download(id=i, output=sheet_id+'_'+str(j)+'.pdf', use_cookies=False, quiet=False)
+                        # output = str(output)
+                        output = sheet_id+'_'+str(j)+'.pdf';
+                        if (obj.FileDownload(i, output)):
+                            if (output is not None):
+                                shutil.move(output, "storage/"+lang+"/"+sheet_id+"/"+output)
+                            j+=1
                 merge(sheet_id, lang)
                 data = {
                     "url" : request.base_url + 'dl/' + lang + '/' + sheet_id + '.pdf'
